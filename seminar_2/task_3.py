70848de1a24b3547622e5f1754d6b8045fef65f1
Yulya <yulyazinina@ya.ru>
2022-11-29 21:26:52 +0800

    Fixed task 3 in seminar 2. Added list

--- seminar_2/task_3.py
+++ seminar_2/task_3.py
@@ -3,8 +3,15 @@
 print('Введите число N')
 number = int(input())
 summ = 0
+list = []
 
-for n in range(1, number):
-    summ += ((1 + 1 / n) ** n)
+for n in range(1, number+1):
+    a = (1 + 1 / n) ** n
+    list.append(round(a, 2))
 
-print(round(summ, 2))
+print(list)
+
+for i in list:
+    summ += i
+
+print(summ)
