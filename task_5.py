2a307a9da671f6dade74037cb0a0cf70f1fd8d94
Yulya <yulyazinina@ya.ru>
2022-11-30 21:20:03 +0800

    Added task 5. Shuffling

---
+++ task_5.py
@@ -0,0 +1,11 @@
+# Реализуйте алгоритм перемешивания списка (shuffle использовать нельзя, другие методы из библиотеки random - можно).
+
+import random
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(len(a)-1, 1, -1):
+    n = random.choice(range(0, i))
+    print(n, i)
+    a[n], a[i] = a[i], a[n]
+
+print(a)
