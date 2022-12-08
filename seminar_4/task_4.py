439191a248a1599e1d1eeacc42a8d01545be339a
Yulya <yulyazinina@ya.ru>
2022-12-08 18:18:36 +0800

    Commented all prints

--- seminar_4/task_4.py
+++ seminar_4/task_4.py
@@ -16,13 +16,7 @@ def get_coefficients_list(k):
 
 
 def get_polynomial(k, coefficients_list):
-    # coefficients = []
     text = ''
-
-    # for i in range(0, k+1):
-    #     coefficients.append(random.choice(range(0, 101)))
-
-    # print(coefficients)
 
     for j in range(0, len(coefficients_list)):
         if j == k:
@@ -36,13 +30,13 @@ def get_polynomial(k, coefficients_list):
 
 
 k = 2
-print(f'степень k = {k}')
+# print(f'степень k = {k}')
 
 coefficients_list_1 = get_coefficients_list(k)
-print(f'coefficients_list_1 = {coefficients_list_1}')
+# print(f'coefficients_list_1 = {coefficients_list_1}')
 
 polynomial_1 = get_polynomial(k, coefficients_list_1)
-print(polynomial_1)
+# print(polynomial_1)
 
 data = open('polynomial.txt', 'w')
 data.write(polynomial_1)
@@ -50,13 +44,13 @@ data.close()
 
 
 m = k
-print(f'степень m = {m}')
+# print(f'степень m = {m}')
 
 coefficients_list_2 = get_coefficients_list(m)
-print(f'coefficients_list_2 = {coefficients_list_2}')
+# print(f'coefficients_list_2 = {coefficients_list_2}')
 
 polynomial_2 = get_polynomial(m, coefficients_list_2)
-print(polynomial_2)
+# print(polynomial_2)
 
 with open('polynomial_2.txt', 'w') as data_2:
     data_2.write(get_polynomial(m, coefficients_list_2))
