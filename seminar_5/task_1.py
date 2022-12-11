2cfca437c5fe1449acc9d931d83c325a52460a21
Yulya <yulyazinina@ya.ru>
2022-12-11 17:22:29 +0800

    Added task 1. Deleting symbols from text file

---
+++ seminar_5/task_1.py
@@ -0,0 +1,13 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+# [‘ПРИВЕТ’, ‘ЗАБВЕНИЕ’, 'ПОКА’] ->[‘ПРИВЕТ’, 'ПОКА’]
+
+
+with open('task_1_text.txt', 'r') as data:
+    text = data.read()
+
+new_list = list(filter(lambda x: ('абв' not in x), text.split()))
+
+with open('task_1_new_text.txt', 'w') as new_data:
+    for i in new_list:
+        new_data.write(i + ' ')
+
